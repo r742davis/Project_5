f1c787da9d05a1fc79b69e7c546c8a6be8342425
Richard <richard.trow.davis@gmail.com>
2019-03-07 22:30:21 -0800

    Connected server to mongoose, added error messages, added comments and section headers. Troubleshooting the mongoURI section

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
+//----------------//
+//  Dependencies  //
+//----------------//
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const db = mongoose.connection
 
 const app = express()
 
-// Body Parser Middleware
+//--------------//
+//  Middleware  //
+//--------------//
 app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
 
-const port = 5000
+//-------------------------//
+//  Environment Variables  //
+//-------------------------//
+const port = process.env.PORT || 5000
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27107/wine"
 
+//---------------------------//
+//  App Listener: Port 5000  //
+//---------------------------//
 app.listen(port, () => {
   console.log('Server is connected on port number: ' + port);
 })
+
+//------------------------//
+//  Connect to Mongooose  //
+//------------------------//
+mongoose.connect(mongoURI, { useNewUrlParser: true },
+  () => { console.log('Mongoose connection established:', mongoURI) }
+)
+//--Error Messages--//
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
