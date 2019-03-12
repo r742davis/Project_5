0047cdca75a6ae7c946bd5a183708de12b3b7010
Richard <richard.trow.davis@gmail.com>
2019-03-11 17:35:28 -0700

    Preparing package.json and server.js for heroku deployment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(methodOverride('_method'))
 //  Environment Variables  //
 //-------------------------//
 const port = process.env.PORT || 5000
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/" + "wine"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/" + "wine"
 
 //---------------------------//
 //  App Listener: Port 5000  //
@@ -34,10 +34,17 @@ app.listen(port, () => {
 //------------------------//
 //  Connect to Mongooose  //
 //------------------------//
-mongoose.connect(mongoURI, { useNewUrlParser: true },
-  () => { console.log('Mongoose connection established:', mongoURI) }
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true },
+  () => { console.log('Mongoose connection established:', MONGODB_URI) }
 )
 //--Error Messages--//
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'))
-db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
 db.on('disconnected', () => console.log('mongo disconnected'))
+
+//------------------//
+//  Routes to Test  //
+//------------------//
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
