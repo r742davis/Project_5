4016afcc87fde890498751b1126f8b2a01074b13
Richard <richard.trow.davis@gmail.com>
2019-03-11 18:11:51 -0700

    Created wine schema in Wine.js

---
+++ models/Wine.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create Schema for Wine
+const WineSchema = new Schema({
+  wineName: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  region: {
+    type: String
+  }
+  alcoholPercentage: {
+    type: Number,
+    required: true
+  },
+  vintage: {
+    type: Number,
+    required: true
+  },
+  producer: {
+    type: String,
+    required: true
+  },
+  reserve: {
+    type: Boolean
+  },
+  estate: {
+    type: Boolean
+  },
+  tastingNotes: {
+    type: String
+  },
+  grape: {
+    type: String
+  },
+  wineScore: {
+    type: Number
+  }
+})
+
+module.exports = Wine = mongoose.model('wine', WineSchema)
