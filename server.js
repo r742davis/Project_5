3ce0f69842a16a814a48ea4dc03b4a73bf585193
Richard <richard.trow.davis@gmail.com>
2019-03-13 14:14:43 -0700

    Removed body parser

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 //----------------//
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const db = mongoose.connection
 const methodOverride = require('method-override')
 const cors = require('cors')
@@ -14,8 +13,8 @@ const app = express()
 //  Middleware  //
 //--------------//
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
